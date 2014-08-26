21f220209472adb8c817a03818a20948dd586647
Derek Neil <derekrneil@gmail.com>
2014-08-26 15:30:36 -0300

    clean up visualization classes

---
+++ Dial.h
@@ -0,0 +1,15 @@
+#ifndef DIAL_H
+#define DIAL_H
+
+#include "universalLogger.h"
+#include "Visualization.h"
+#include "SensorData.h"
+#include "DisplayElement.h"
+#include "Display.h"
+
+class Dial : public Visualization {
+
+	/** visualization for none historical visualization ie, volume dial */
+
+};
+#endif
